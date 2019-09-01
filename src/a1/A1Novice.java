b98620351b254ac19a465a598476030cea6c8519
kelseysnelgrove <kelseysnelgrove@kelseys-mbp.lan>
2019-09-01 12:56:00 -0400

    Created code for novice level

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,35 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		process(scan);
+		
+	}
+	
+	public static void process(Scanner scan) {
+		
+			int countOfCustomers = scan.nextInt();
+		
+		for (int i = 0; i < countOfCustomers; i++) {
+			String firstName;
+			firstName = scan.next();
+			String lastName;
+			lastName = scan.next();
+			int numberOfTotalItems;
+			numberOfTotalItems = scan.nextInt();
+			double totalPrice = 0;
+			
+			for (int j = 0; j < numberOfTotalItems; j++) {
+				int numberOfEachItem;
+				
+				numberOfEachItem = scan.nextInt();
+				String nameOfEachItem;
+				nameOfEachItem = scan.next();
+				double priceOfEachItem;
+				priceOfEachItem = scan.nextDouble();
+				totalPrice += numberOfEachItem * priceOfEachItem;
+			}
+			System.out.println(firstName.substring(0,1) + ". " + lastName + ": " + String.format("%.2f", totalPrice));
+		}
 		
 	}
 }
